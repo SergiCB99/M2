a1a053222bca2b6e43259d2d1e7d1b529cd357ee
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-07-20 20:07:31 +0200

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,14 @@
+
+public class Employee extends Persona {
+
+	public Employee (String nombre,Double salario_bruto_mensual) {
+		super(nombre,salario_bruto_mensual);
+		this.salario_bruto_mensual=this.comprobarSalario()-(this.comprobarSalario()*0.15);
+		this.salario_neto_mensual=this.salarioNeto();
+		this.salario_bruto_anual=(this.salario_bruto_mensual*12);
+		this.salario_neto_anual=(this.salario_neto_mensual*12);
+	}
+
+	
+	
+}
